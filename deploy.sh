88c60b3d7ec68bfae09829adbfa040c8b7ba60de
maqingbo <wmaqingbo@163.com>
2022-05-04 21:35:13 +0800

    chore: 修改说明；

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:qb-ebook/wmlyscdyy.git master:gh-pages
+git push -f git@github.com:qb-eBook/yfjlb.git master:gh-pages
 
 cd -
--- docs/README.md
+++ docs/README.md
@@ -1,9 +1,9 @@
 ---
 home: true
 heroImage: /images/cover.jpg
-heroText: 语法俱乐部
+heroText: 英语魔法师之语法俱乐部
 tagline: '旋元佑'
 actionText: 开始阅读
 actionLink: /chapters/01/
-# footer: MIT Licensed | Copyright © 2018-present 上野千鹤子
+# footer: MIT Licensed | Copyright © 2018-present 旋元佑
 ---
